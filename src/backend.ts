fc9ab235cd42f4e3f868750abbab403c50c7930c
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-12-02 13:57:02 +0700

    3rd Commit

---
+++ src/backend.ts
@@ -0,0 +1,110 @@
+import { time, period } from "./data/schedule.json";
+
+export enum PeriodStatus {
+  IN_PERIOD = 0,
+  IN_BREAK = 1,
+  ENDED = 2,
+}
+
+export interface PeriodInfo {
+  name: string;
+  teacher: string;
+  time: string;
+}
+
+export interface Period {
+  status: PeriodStatus;
+  // * If IN_PERIOD, then this is current period
+  // * If IN_BREAK, then this is next period
+  // * If ENDED, then this is undefined
+  period?: number;
+  info?: PeriodInfo;
+  nextPeriod?: PeriodInfo;
+}
+
+export function getPeriod() {
+  const dtnow = new Date();
+  const now = `${dtnow.getHours()}:${dtnow.getMinutes()}`;
+
+  let periodIndex = 1;
+  while (true) {
+    // @ts-ignore
+    if (!period[`${dtnow.getDay()}:${period}`]) {
+      return {
+        status: PeriodStatus.ENDED,
+      };
+    }
+
+    // @ts-ignore
+    const [start, end] = time[period].split("-");
+    if (cmpTime(end, now) <= 0) {
+      periodIndex++;
+      continue;
+    }
+
+    if (withinPeriod(periodIndex, dtnow)) {
+      return {
+        status: PeriodStatus.IN_PERIOD,
+        period: periodIndex,
+        info: getPeriodInfo(periodIndex),
+        // @ts-ignore
+        nextPeriod: period[`${dtnow.getDay()}:${period}`]
+          ? getPeriodInfo(periodIndex + 1)
+          : undefined,
+      };
+    } else {
+      return {
+        status: PeriodStatus.IN_BREAK,
+        period: periodIndex,
+        info: getPeriodInfo(periodIndex),
+      };
+    }
+  }
+}
+
+function getPeriodInfo(p: number) {
+  const day = new Date().getDay();
+  // @ts-ignore
+  const pinfo = period[`${day}:${p}`];
+
+  return {
+    name: pinfo.name,
+    teacher: pinfo.teacher,
+    // @ts-ignore
+    time: time[p],
+  };
+}
+
+function withinPeriod(p: number, dtnow: Date): boolean {
+  const now = `${dtnow.getHours}:${dtnow.getMinutes}`;
+
+  // @ts-ignore
+  if (!time[p]) {
+    return false;
+  }
+
+  // @ts-ignore
+  const [start, end] = time[p].split("-");
+
+  return cmpTime(now, start) >= 0 && cmpTime(now, end) <= 0;
+}
+
+// * By GitHub Copilot
+function cmpTime(a: string, b: string): number {
+  const [ah, am] = a.split(":");
+  const [bh, bm] = b.split(":");
+
+  if (+ah > +bh) {
+    return 1;
+  } else if (+ah < +bh) {
+    return -1;
+  } else {
+    if (+am > +bm) {
+      return 1;
+    } else if (+am < +bm) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+}
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,21 +1,43 @@
 import "./global.scss";
 import "./popup.scss";
 
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+
+import { getPeriod, PeriodStatus } from "./backend";
 
 const Popup: FC = () => {
   const [time, setTime] = useState(new Date());
 
-  setInterval(() => {
-    setTime(new Date());
-  }, 1000);
+  const day = time.getDay();
+  const currentPeriod = getPeriod();
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+  }, []);
 
   return (
     <div className="popup">
-      <ul>
-        <li>Current Time: {time.toLocaleString()}</li>
-      </ul>
+      <h1>Current Time: {time.toLocaleString()}</h1>
+      {currentPeriod.status == PeriodStatus.IN_PERIOD ? (
+        <>
+          <h1>Current Period: {currentPeriod.period}</h1>
+          <h1>Subject: {currentPeriod.info!.name}</h1>
+          <h1>Teacher: {currentPeriod.info!.teacher}</h1>
+          <h3>{currentPeriod.info!.time}</h3>
+          {currentPeriod.nextPeriod ? (
+            <h1>Next Period: {currentPeriod.nextPeriod.name}</h1>
+          ) : (
+            <h1>No Next Period</h1>
+          )}
+        </>
+      ) : currentPeriod.status == PeriodStatus.IN_BREAK ? (
+        <h1>Break</h1>
+      ) : (
+        <h1>Ended</h1>
+      )}
     </div>
   );
 };
