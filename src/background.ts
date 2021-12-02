349703a678934f61d0ded12ebdf990aed5d3ee4b
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-12-02 11:55:37 +0700

    2nd Commit

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,13 @@
-function polling() {
-  // console.log("polling");
-  setTimeout(polling, 1000 * 30);
+function notify() {
+  chrome.notifications.create("69420", {
+    type: "basic",
+    iconUrl: "icon.png",
+    title: "notification title",
+    message: "notification message",
+    priority: 2,
+  });
 }
 
-polling();
+const nInterval = setInterval(() => {
+  notify();
+}, 1000 * 10);
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,51 +1,22 @@
-import React, { useEffect, useState } from "react";
+import "./global.scss";
+import "./popup.scss";
+
+import React, { FC, useState } from "react";
 import ReactDOM from "react-dom";
 
-const Popup = () => {
-  const [count, setCount] = useState(0);
-  const [currentURL, setCurrentURL] = useState<string>();
+const Popup: FC = () => {
+  const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
-    chrome.action.setBadgeText({ text: count.toString() });
-  }, [count]);
-
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      setCurrentURL(tabs[0].url);
-    });
-  }, []);
-
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const tab = tabs[0];
-      if (tab.id) {
-        chrome.tabs.sendMessage(
-          tab.id,
-          {
-            color: "#555555",
-          },
-          (msg) => {
-            console.log("result message:", msg);
-          }
-        );
-      }
-    });
-  };
+  setInterval(() => {
+    setTime(new Date());
+  }, 1000);
 
   return (
-    <>
-      <ul style={{ minWidth: "700px" }}>
-        <li>Current URL: {currentURL}</li>
-        <li>Current Time: {new Date().toLocaleTimeString()}</li>
+    <div className="popup">
+      <ul>
+        <li>Current Time: {time.toLocaleString()}</li>
       </ul>
-      <button
-        onClick={() => setCount(count + 1)}
-        style={{ marginRight: "5px" }}
-      >
-        count up
-      </button>
-      <button onClick={changeBackground}>change background</button>
-    </>
+    </div>
   );
 };
 
